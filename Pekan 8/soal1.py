9e13d344d19073ebbe9ce08b09a23d371408236c
Aldinokhalifah <146077793+Aldinokhalifah@users.noreply.github.com>
2023-11-22 10:26:07 +0700

    Add files via upload

---
+++ Pekan 8/soal1.py
@@ -0,0 +1,7 @@
+def profil(nama,alamat,gender,umur,hoby):
+    print("Nama saya adalah",nama)
+    print("Alamat saya yaitu",alamat)
+    print("Gender saya adalah",gender)
+    print("Umur saya adalah",umur)
+    print("Hobi saya adalah",hoby)
+profil("Aldino","Jln.Sukamulia5","Laki-laki","18","Olahraga")
---
+++ Pekan 8/soal3.py
@@ -0,0 +1,7 @@
+def cari_ganjil(n):
+    for i in range(1,n+1,2):
+        print(i)
+
+cari_ganjil(100)
+
+        
